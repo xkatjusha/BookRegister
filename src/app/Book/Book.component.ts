00c0d076282b12e661ccfccb87c4861c81b66a18
xkatjusha <katharina.maier.25@web.de>
2022-11-22 13:44:54 +0100

    created new component named Book

---
+++ src/app/Book/Book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Book',
+  templateUrl: './Book.component.html',
+  styleUrls: ['./Book.component.css']
+})
+export class BookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BookComponent } from './Book/Book.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      BookComponent
+   ],
   imports: [
     BrowserModule
   ],
